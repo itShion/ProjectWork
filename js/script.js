40ed4015161c0cf585302228de14a1de1127cb96
maminaou <aouachimohammedamin@istitutocorni.it>
2025-04-13 20:06:26 +0200

    1st commit - added 3 grids with animation

--- js/script.js
+++ js/script.js
@@ -6,3 +6,24 @@ menuBtn.addEventListener('click', () => {
     navOverlay.style.opacity = navOverlay.style.opacity === '1' ? '0' : '1';
     navOverlay.style.pointerEvents = navOverlay.style.pointerEvents === 'all' ? 'none' : 'all';
 });
+
+// ----------------------------------MAMINS CODE----------------------------------------- 
+document.querySelectorAll('.feature-grid').forEach(grid => {
+    grid.addEventListener('mouseenter', () => {
+        const content = grid.querySelector('.feature-content');
+        const name = content.querySelector('.device-name');
+        const details = content.querySelector('.device-details');
+        
+        name.style.display = 'none';
+        details.style.display = 'block';
+    });
+    
+    grid.addEventListener('mouseleave', () => {
+        const content = grid.querySelector('.feature-content');
+        const name = content.querySelector('.device-name');
+        const details = content.querySelector('.device-details');
+        
+        name.style.display = 'block';
+        details.style.display = 'none';
+    });
+});
